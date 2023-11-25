eb7dc310ab603e860a4323ac6f215da555c79a0d
Valentina Nasanovich <127026317+vnasanovich@users.noreply.github.com>
2023-11-25 12:29:38 +0300

    feat: add timer functionality

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
 const startBtn = document.querySelector("#start");
 const screens = document.querySelectorAll(".screen");
+const timeList = document.querySelector("#time-list");
+const timeElement = document.querySelector("#time");
+let time = 0;
 startBtn.addEventListener("click", (event) => {
   event.preventDefault();
   screens[0].classList.add("up");
 });
+timeList.addEventListener("click", (event) => {
+  if (event.target.classList.contains("time-btn")) {
+    time = parseInt(event.target.getAttribute("data-time"));
+    screens[1].classList.add("up");
+    startGame();
+  }
+});
+function startGame() {
+  setInterval(decreaseTime, 1000);
+  setTime(time);
+}
+function decreaseTime() {
+  if (time === 0) {
+    finishGame();
+  } else {
+    let current = --time;
+    if (current < 10) {
+      current = `0${current}`;
+    }
+    setTime(current);
+  }
+}
+function setTime(value) {
+  timeElement.innerHTML = `00:${value}`;
+}
+function finishGame() {
+}
